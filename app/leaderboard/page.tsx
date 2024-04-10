377663516a0d45e5dc2f92f6e6983e017ae26ace
Justin Byrd <justinbyrd7@gmail.com>
2024-04-10 17:24:17 -0500

    added leaderboard page

---
+++ app/leaderboard/page.tsx
@@ -0,0 +1,10 @@
+export default function Leaderboard() {
+  return (
+    <main className="flex flex-col min-h-screen">
+      <h1>Leaderboard</h1>
+      <div>Home Icon</div>
+      <div>Tabs from shadcn/ui</div>
+      <div>Mapped leaders and scores</div>
+    </main>
+  );
+}
