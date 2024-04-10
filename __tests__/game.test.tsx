18e5cf41e790520b2d623cf39b20e885b4471a8a
Justin Byrd <justinbyrd7@gmail.com>
2024-04-10 15:53:07 -0500

    Add Nav component and update tests

--- __tests__/game.test.tsx
+++ __tests__/game.test.tsx
@@ -3,32 +3,63 @@ import { render, screen } from "@testing-library/react";
 import Game from "@/app/game/page";
 
 describe("Game Page", () => {
-  it("score tracker renders", () => {});
+  //// Score Tracker
+  it("score tracker renders", () => {
+    render(<Game />);
+    expect(false).toBe(true);
+  });
+
+  //// Timer
   it("timer renders", () => {
+    render(<Game />);
     expect(false).toBe(true);
   });
+
+  //// Home Icon
   it("home icon renders", () => {
+    render(<Game />);
     expect(false).toBe(true);
   });
+
+  //// Team Logo
   it("team logo renders", () => {
+    render(<Game />);
     expect(false).toBe(true);
   });
+
+  //// Answer 1
   it("answer 1 renders", () => {
+    render(<Game />);
     expect(false).toBe(true);
   });
+
+  //// Answer 2
   it("answer 2 renders", () => {
+    render(<Game />);
     expect(false).toBe(true);
   });
+
+  //// Answer 3
   it("answer 3 renders", () => {
+    render(<Game />);
     expect(false).toBe(true);
   });
+
+  //// Answer 4
   it("answer 4 renders", () => {
+    render(<Game />);
     expect(false).toBe(true);
   });
+
+  //// Correct Answer
   it("answers is green if correct", () => {
+    render(<Game />);
     expect(false).toBe(true);
   });
+
+  //// Incorrect Answer
   it("answer is red if correct", () => {
+    render(<Game />);
     expect(false).toBe(true);
   });
 });
--- app/game/page.tsx
+++ app/game/page.tsx
@@ -1,13 +1,18 @@
-import Image from "next/image";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
-import { StartButton } from "@/components/Buttons";
+import Nav from "@/components/Nav";
 
 export default function Game() {
-  return <main className="flex flex-col min-h-screen p-2"></main>;
+  return (
+    <main className="flex flex-col min-h-screen p-2">
+      <Nav />
+      <div>Score</div>
+      <div>Timer</div>
+      <div>Home</div>
+      <div>Team Logo</div>
+      <div>Answers</div>
+      <div>Answer 1</div>
+      <div>Answer 2</div>
+      <div>Answer 3</div>
+      <div>Answer 4</div>
+    </main>
+  );
 }
