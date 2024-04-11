820c4bcdff3bb35638cc96c154196a4b6c2c0b10
Justin Byrd <justinbyrd7@gmail.com>
2024-04-11 15:10:34 -0500

    Fix broken link in Nav component

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,12 +1,15 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import Link from "next/link";
 
 export default function Nav() {
   return (
     <nav>
+      {/* <Link href="/profile"> */}
       <Avatar>
         <AvatarImage src="https://github.com/shadcn.png" />
         <AvatarFallback>CN</AvatarFallback>
       </Avatar>
+      {/* </Link> */}
     </nav>
   );
 }
