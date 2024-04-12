8fee23820e58f3f86fa99463aa077fb5001149f1
Justin Byrd <justinbyrd7@gmail.com>
2024-04-12 10:35:52 -0500

    debugging app tests

--- jest.config.ts
+++ jest.config.ts
@@ -108,7 +108,7 @@ const config: Config = {
   // notifyMode: "failure-change",
 
   // A preset that is used as a base for Jest's configuration
-  // preset: undefined,
+  preset: "ts-jest",
 
   // Run tests from one or more projects
   // projects: undefined,
@@ -195,7 +195,7 @@ const config: Config = {
   // unmockedModulePathPatterns: undefined,
 
   // Indicates whether each individual test should be reported during the run
-  // verbose: undefined,
+  verbose: true,
 
   // An array of regexp patterns that are matched against all source file paths before re-running tests in watch mode
   // watchPathIgnorePatterns: [],
--- jest.setup.ts
+++ jest.setup.ts
@@ -1,10 +1,1 @@
-import React from "react";
 import "@testing-library/jest-dom";
-
-// jest.mock("next/image", () => ({
-//   __esModule: true,
-//   default: (props: any): React.ReactElement => {
-//     // eslint-disable-next-line @next/next/no-img-element
-//     return React.createElement("img", props);
-//   },
-// }));
