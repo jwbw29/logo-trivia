9893c3258a74d8cbb5f40c0a9989ccc3ca68bdcb
Justin Byrd <justinbyrd7@gmail.com>
2024-04-13 09:14:52 -0500

    Update styling in game page and add pagination to LeaderboardTabs component

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
   return (
     <main className="flex flex-col min-h-screen">
       <Nav />
-      <div className="flex flex-col flex-1 justify-center items-center gap-24 mb-[104px] m-16">
+      <div className="flex flex-col flex-1 justify-start items-center gap-8 mb-[104px]">
         <Image
           src={logo}
           alt="game logo"
