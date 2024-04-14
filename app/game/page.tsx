063619e6841e43b5b7089cf007ec1be52e866ee8
Justin Byrd <justinbyrd7@gmail.com>
2024-04-14 15:16:28 -0500

    changed exit button to settings

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -1,13 +1,15 @@
-import { QuitButton } from "../../components/Buttons";
 import Image from "next/image";
 import mizzou from "@/public/mockData/mizzouLogo.png";
 import { Button } from "@/components/ui/button";
+import { LuSettings } from "react-icons/lu";
 
 export default function Game() {
   return (
     <main className="testBorder flex flex-col min-h-screen">
       <div className="flex justify-end p-2 pt-4">
-        <QuitButton />{" "}
+        <Button variant="ghost" size="icon">
+          <LuSettings className="text-2xl" />
+        </Button>
       </div>
       <div className="flex justify-between text-3xl font-oxanium p-10">
         <div className="border border-border rounded-sm py-4 px-6">0 / 0</div>
