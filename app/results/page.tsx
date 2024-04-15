5fe56407ee4a74acc9bf41b1ba8659e584a07282
Justin Byrd <justinbyrd7@gmail.com>
2024-04-14 21:03:00 -0500

    Refactor game page to add Nice! message and login functionality

--- app/results/page.tsx
+++ app/results/page.tsx
@@ -1,16 +1,13 @@
+import { LeaderboardTabs } from "@/components/LeaderboardTabs";
+import Login from "@/components/Login";
+
 export default function Results() {
   return (
     <main className="flex flex-col min-h-screen">
       <div>Home Icon</div>
-      <div>Final Score Message</div>
-      <div>Final score</div>
-      <div>Log in to submit score</div>
-      <form>
-        <div>email input</div>
-        <div>password input</div>
-        <div>login button</div>
-        <div>register ghost button</div>
-      </form>
+      <div>Nice!</div>
+      <div>10/10</div>
+      {true ? <Login /> : <LeaderboardTabs />}{" "}
     </main>
   );
 }
---
+++ components/Login.tsx
@@ -0,0 +1,11 @@
+export default function Login() {
+  return (
+    <div>
+      <div>Log in to submit score</div>
+      <div>email</div>
+      <div>password</div>
+      <div>login button</div>
+      <div>signup</div>
+    </div>
+  );
+}
