48e1b86d37258c20a05e804f6680c0f66e0dce7f
Justin Byrd <justinbyrd7@gmail.com>
2024-04-15 14:58:51 -0500

    Update result message and score display in Results page

--- app/results/page.tsx
+++ app/results/page.tsx
@@ -7,6 +7,28 @@ import Link from "next/link";
 export default function Results() {
   let signedIn = true;
 
+  let score = 10;
+  let attempts = 10;
+  let message = "";
+
+  switch (true) {
+    case score >= 0 && score <= 1:
+      message = "Uh...";
+      break;
+    case score >= 2 && score <= 4:
+      message = "Try again, maybe?";
+      break;
+    case score >= 5 && score <= 7:
+      message = "Not bad!";
+      break;
+    case score >= 8 && score <= 10:
+      message = "Nice!";
+      break;
+    default:
+      message = "Invalid score";
+      break;
+  }
+
   return (
     <main className="flex flex-col min-h-screen ">
       <nav className="flex justify-end p-8 ">
@@ -18,19 +40,16 @@ export default function Results() {
       </nav>
       <div className="flex flex-col justify-center items-center gap-4 p-4">
         <div aria-label="result message">
-          {/* 
-          - if score = 0-1, message = 'Uh...'
-          - if score = 2-4, message = 'Try again, maybe?'
-          - if score = 5-7, message = 'Not bad!'
-          - if score = 8-10, message = 'Nice!' */}
-          <h1 className="text-6xl font-caveat text-primary">Nice!</h1>
+          <h1 className="text-6xl font-caveat text-primary">{message}</h1>
         </div>
         <div
           aria-label="result score"
           className="flex items-center justify-center "
         >
           {" "}
-          <h1 className="text-8xl font-caveat ">10/10 </h1>
+          <h1 className="text-8xl font-caveat ">
+            {score} / {attempts}{" "}
+          </h1>
         </div>
       </div>
       <div className="flex self-center justify-center items-center my-8 w-5/6">
