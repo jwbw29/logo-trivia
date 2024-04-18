32b15bb5ee27080df07b3f2b52c66f983ba1874f
Justin Byrd <justinbyrd7@gmail.com>
2024-04-18 14:01:47 -0500

    user insert working

--- components/Username.tsx
+++ components/Username.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useState } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
@@ -10,36 +11,79 @@ import {
   FormItem,
   FormControl,
   FormMessage,
+  FormDescription,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { createClient } from "@/utils/supabase/client";
 
 const formSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
+  username: z.string().min(6, {
+    message: "Username must be at least 6 characters.",
   }),
 });
 
 export default function Username() {
+  const [loading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [userCreated, setUserCreated] = useState(false);
+
   // 1. Define your form.
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       username: "",
     },
+    mode: "onChange",
   });
 
+  const { isValid } = form.formState;
+
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values);
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    setLoading(true);
+    setErrorMessage("");
+    const supabase = createClient();
+
+    // Fetch users to check if username is already taken
+    const { data: users, error } = await supabase
+      .from("users")
+      .select("username")
+      .eq("username", values.username)
+      .single();
+
+    if (error && error.message !== "No rows found") {
+      setErrorMessage("An error occurred, please try again.");
+      setLoading(false);
+      return;
+    }
+
+    if (users) {
+      setErrorMessage("Username is already taken.");
+      setLoading(false);
+      return;
+    }
+
+    // Insert username into the database
+    const { error: insertError } = await supabase
+      .from("users")
+      .insert([{ username: values.username }]);
+
+    if (insertError) {
+      setErrorMessage(insertError.message);
+    } else {
+      // Redirect or perform further actions
+      setUserCreated(true);
+      console.log("Username registered successfully!");
+    }
+
+    setLoading(false);
   }
 
   return (
     <Form {...form}>
       <form
         onSubmit={form.handleSubmit(onSubmit)}
-        className="flex w-full items-center justify-end gap-2 mt-16"
+        className="flex w-full justify-end gap-2 mt-16"
       >
         <FormField
           control={form.control}
@@ -55,10 +99,9 @@ export default function Username() {
         />
         <Button
           aria-label="login button"
-          // TODO enable button when username meets requirements
-          disabled
+          disabled={!isValid || loading} // TODO is this right? or should it be based off length requirement being met?
           // size="xxl"
-          className="tracking-[.2rem]"
+          className="tracking-[.2rem] self-start"
         >
           PLAY
         </Button>
