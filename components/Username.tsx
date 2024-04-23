db8619c335ebe5a548e6c35aa2f395ea66f34b90
Justin Byrd <justinbyrd7@gmail.com>
2024-04-23 12:54:24 -0500

    moving username component into app

--- components/Username.tsx
+++ components/Username.tsx
@@ -13,6 +13,16 @@ import {
   FormDescription,
   FormMessage,
 } from "@/components/ui/form";
+import Link from "next/link";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+  SelectGroup,
+  SelectLabel,
+} from "@/components/ui/select";
 import { Input } from "@/components/ui/input";
 import { createClient } from "@/utils/supabase/client";
 
@@ -22,13 +32,14 @@ const formSchema = z.object({
   }),
 });
 
-export default function Username() {
+const supabase = createClient();
 
+export default function Username() {
   const [loading, setLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
   const [userCreated, setUserCreated] = useState(false);
 
-  // Define your form.
+  //// Define your form.
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -38,21 +49,26 @@ export default function Username() {
   });
   const { isValid } = form.formState;
 
-  // Define a submit handler.
+  //// Define a submit handler.
   async function authenticateAndSaveUsername(
     values: z.infer<typeof formSchema>
   ) {
     setLoading(true);
     setErrorMessage("");
-    const supabase = createClient();
 
-    // Fetch users to check if username is already taken
+    //// Fetch users to check if username is already taken
     const { data: existingUser, error: usernameError } = await supabase
-      .from("users")
+      .from("profiles")
       .select("username")
       .eq("username", values.username)
       .single();
 
+    existingUser
+      ? console.log("existing user", existingUser)
+      : console.log("existing user is null");
+
+    //// if we get an error and that error is NOT "JSON object requested, multiple (or no) rows returned", then we have a problem
+    //// otherwise we have a unique user
     if (
       usernameError &&
       usernameError.message !==
@@ -65,18 +81,106 @@ export default function Username() {
       console.log("unique user entered!");
     }
 
-    if (existingUser) {
+    if (!existingUser) {
+      //// if user doesn't exist create one with the username provided and tie it to the current session user, then update the state so that username shows at the top instead of the username component
+      const session = (await supabase.auth.getSession()).data.session;
+      const userId = session?.user.id;
+      const { data: newUser, error: newUserError } = await supabase
+        .from("profiles")
+        .insert([{ username: values.username, id: userId }]);
+
+      if (newUserError) {
+        setErrorMessage("An error occurred, please try again.");
+        setLoading(false);
+        return;
+      }
+
+      //// clear input box
+      form.setValue("username", "");
+
+      setUserCreated(true);
+    } else {
       setErrorMessage("Username is already taken.");
       setLoading(false);
       return;
     }
 
-    // TODO UPDATE current session
+    // TODO once we have a username, push it to the leaderboard database
+    console.log("userCreated: ", userCreated);
 
     setLoading(false);
   }
 
-  return (
+  return userCreated && !loading ? (
+    <div className="flex flex-col gap-12">
+      <Select>
+        <SelectTrigger aria-label="category select" className="w-80">
+          <SelectValue placeholder="Choose Category" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="all">All Teams</SelectItem>
+          <SelectGroup>
+            <SelectLabel className="text-primary ">NBA</SelectLabel>
+            <SelectItem value="nba-all">All</SelectItem>
+            <SelectItem value="nba-east">Eastern Conference</SelectItem>
+            <SelectItem value="nba-west">Western Conference</SelectItem>
+          </SelectGroup>
+          <SelectGroup>
+            <SelectLabel className="text-primary">NFL</SelectLabel>
+            <SelectItem value="nfl-all">All</SelectItem>
+            <SelectItem value="nfl-afc">AFC</SelectItem>
+            <SelectItem value="nfl-nfc">NFC</SelectItem>
+          </SelectGroup>
+          so
+          <SelectGroup>
+            <SelectLabel className="text-primary">MLB</SelectLabel>
+            <SelectItem value="mlb-all">All</SelectItem>
+            <SelectItem value="mlb-al">American League</SelectItem>
+            <SelectItem value="mlb-nl">National League</SelectItem>
+          </SelectGroup>
+          <SelectGroup>
+            <SelectLabel className="text-primary">NHL</SelectLabel>
+            <SelectItem value="nhl-all">All</SelectItem>
+            <SelectItem value="nhl-east">Eastern Conference</SelectItem>
+            <SelectItem value="nhl-west">Western Conference</SelectItem>
+          </SelectGroup>
+          <SelectGroup>
+            <SelectLabel className="text-primary">MLS</SelectLabel>
+            <SelectItem value="mls-all">All</SelectItem>
+            <SelectItem value="mls-east">Eastern Conference</SelectItem>
+            <SelectItem value="mls-west">Western Conference</SelectItem>
+          </SelectGroup>
+          <SelectGroup>
+            <SelectLabel className="text-primary">WNBA</SelectLabel>
+            <SelectItem value="wnba-all">All</SelectItem>
+            <SelectItem value="wnba-east">Eastern Conference</SelectItem>
+            <SelectItem value="wnba-west">Western Conference</SelectItem>
+          </SelectGroup>
+          <SelectGroup>
+            <SelectLabel className="text-primary">NCAA</SelectLabel>
+            <SelectItem value="ncaa-all">All</SelectItem>
+            <SelectItem value="ncaa-acc">ACC</SelectItem>
+            <SelectItem value="ncaa-big10">Big Ten</SelectItem>
+            <SelectItem value="ncaa-big12">Big 12</SelectItem>
+            <SelectItem value="ncaa-sec">SEC</SelectItem>
+          </SelectGroup>
+          <SelectGroup>
+            <SelectLabel className="text-primary">States</SelectLabel>
+            <SelectItem value="states">States</SelectItem>
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+      <Button
+        aria-label="start button"
+        variant="default"
+        size="xl"
+        className="text-base tracking-[.2rem] w-full"
+        asChild
+      >
+        <Link href="/play">START</Link>
+      </Button>
+    </div>
+  ) : (
     <Form {...form}>
       <form
         onSubmit={form.handleSubmit(authenticateAndSaveUsername)}
