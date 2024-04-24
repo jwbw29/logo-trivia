4639597aa251e74dc39e58d99cb112f496e67dcf
Justin Byrd <justinbyrd7@gmail.com>
2024-04-24 10:07:01 -0500

    Refactor variable names and add username submission functionality in Results component

--- app/play/results/page.tsx
+++ app/play/results/page.tsx
@@ -33,9 +33,9 @@ export default function Results() {
   const [errorMessage, setErrorMessage] = useState("");
 
   //// Declare variables
-  let score = 26; // TODO fetch this from game session
+  let correct = 26; // TODO fetch this from game session
   let attempts = 28; // TODO fetch this from game session
-  let percent = score / attempts;
+  let percent = correct / attempts;
   let percentString = (percent * 100).toFixed(2) + "%";
   let message = "";
 
@@ -72,11 +72,31 @@ export default function Results() {
   const onSave = async (data: z.infer<typeof formSchema>) => {
     setLoading(true);
     setErrorMessage("");
-    form.reset();
+    setUsername(data.username);
+    console.log("username after setting username", username);
+    // form.reset();
     // insert into leaderboard
+    // const { data: insertedUser, error: insertUserError } = await supabase
+    //   .from("leaderboard")
+    //   .insert({
+    //     username: data.username,
+    //     correct_answers: correct,
+    //     attempts: attempts,
+    //   });
+
+    // if (insertUserError) {
+    //   setErrorMessage("Error submitting username. Please try again.");
+    //   console.error("Error submitting username: ", insertUserError);
+    //   setLoading(false);
+    //   return;
+    // }
+
+    // if (insertedUser) {
+    //   console.log("inserted user: ", insertedUser);
+    // }
     // confirmation on success
     // disable input once username is saved
-    console.log("saved username: ", data.username); // refresh leaderboard (this may be done somewhere else)
+    console.log("saved username: ", username); // refresh leaderboard (this may be done somewhere else)
     setLoading(false);
   };
 
@@ -96,53 +116,60 @@ export default function Results() {
         >
           {" "}
           <h1 className="text-4xl md:text-6xl font-caveat ">
-            {score} / {attempts}{" "}
+            {correct} / {attempts}{" "}
           </h1>
           <h1 className="text-8xl font-caveat ">{percentString} </h1>
         </div>
       </div>
       <div className="flex flex-col self-center justify-center items-center my-4 w-5/6 gap-8">
-        <Form {...form}>
-          <form
-            onSubmit={form.handleSubmit(onSave)}
-            className="flex flex-col items-start gap-4 w-full py-2"
-          >
-            <FormDescription>
-              Enter username to add score to leaderboard
-            </FormDescription>
-            <div className="flex w-full items-end gap-2">
-              <FormField
-                control={form.control}
-                name="username"
-                render={({ field }) => (
-                  <FormItem className="flex-1">
-                    <FormControl>
-                      <Input
-                        placeholder="Username"
-                        {...field}
-                        disabled={loading}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                    {errorMessage && (
-                      <div className="text-sm font-medium text-destructive">
-                        {errorMessage}
-                      </div>
-                    )}
-                  </FormItem>
-                )}
-              />
-              <Button
-                aria-label="save username button"
-                disabled={!isValid || loading} // TODO is this right? or should it be based off length requirement being met?
-                // size="xxl"
-                className="tracking-[.2rem] self-start"
-              >
-                SAVE
-              </Button>
-            </div>
-          </form>
-        </Form>
+        {username ? (
+          <p>
+            Added <span className="text-red-500">{username}</span> to
+            Leaderboard
+          </p>
+        ) : (
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSave)}
+              className="flex flex-col items-start gap-4 w-full py-2"
+            >
+              <FormDescription>
+                Enter username to add score to leaderboard
+              </FormDescription>
+              <div className="flex w-full items-end gap-2">
+                <FormField
+                  control={form.control}
+                  name="username"
+                  render={({ field }) => (
+                    <FormItem className="flex-1">
+                      <FormControl>
+                        <Input
+                          placeholder="Username"
+                          {...field}
+                          disabled={loading}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                      {errorMessage && (
+                        <div className="text-sm font-medium text-destructive">
+                          {errorMessage}
+                        </div>
+                      )}
+                    </FormItem>
+                  )}
+                />
+                <Button
+                  aria-label="save username button"
+                  disabled={!isValid || loading} // TODO is this right? or should it be based off length requirement being met?
+                  // size="xxl"
+                  className="tracking-[.2rem] self-start"
+                >
+                  SAVE
+                </Button>
+              </div>
+            </form>
+          </Form>
+        )}{" "}
         <LeaderboardTabs />
       </div>
     </main>
